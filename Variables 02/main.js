1e032f1a7c5f61a103c2d37be123890475de7273
Saboor-Hamedi <janhamedi49@gmail.com>
2023-10-22 10:51:57 +0700

    variable update

--- Variables 02/main.js
+++ Variables 02/main.js
@@ -2,22 +2,25 @@
 /*  A variable is a “named storage” for data. 
  *  We can use variables to store goodies, visitors,
  *  and other data.
+ * var
+ * let 
+ * const
 */
 
- let message; 
- message = 'Hello, I am variable';
- console.log(message);
+let message;
+message = 'Hello, I am variable';
+console.log(message);
 
- // change certain words 
- let name ='Saboor';
-console.log('I would soon replaced by '+name);
-const number= 1; // unchagable 
+// change certain words 
+let name = 'Saboor';
+console.log('I would soon replaced by ' + name);
+const number = 1; // unchagable 
 // console.log(number = 2); Assignment to constant variable
 
-let x=2;
-let y=3;
+let x = 2;
+let y = 3;
 let result = eval('x + y'); // The eval() function in JavaScript can be used to evaluate mathematical expressions written as strings.
-console.log(result); 
+console.log(result);
 
 let expression = '5+2+6-3*4';
 let result1 = eval(expression);
@@ -36,4 +39,48 @@ let color = COLOR_ORANGE;
 console.log(color);
 // time to load page
 
+// Re-assigning variable 
+let age = 30;
+console.log(age)
+// re-assign it again
+age = 40;
+console.log(age);
 
+// declare 
+let score;
+score = 10;
+console.log(score);
+if (true) {
+    score = score - 1
+}
+console.log(score);
+// we cannot do this with const 
+
+while(score > 0){
+    score = score -1;
+}
+console.log('You lost');
+// const
+
+const xx = 100;
+console.log(xx);
+const arr = [1,2,3,4]
+arr.push(5);
+console.log(arr);
+// same goes with object 
+const people = {
+    name: 'saboor', 
+    lastname: 'hamedi'
+};
+console.log(people);
+// or you can like this 
+
+people.name = 'Hello';
+people.email = 'x@gmail.com';
+console.log(people);
+
+//multiple variable 
+let var1, var2,var3;
+
+var1 = 10;
+console.log(var1);
